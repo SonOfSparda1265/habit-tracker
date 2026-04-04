253f7374efd6362e5ec161f3e920b74af7d61249
Igor <covigor21909@gmail.com>
2026-04-04 21:07:27 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,59 @@
+function createHabitItem(habitText){
+    let li = document.createElement("li");
+        li.dataset.name = habitText;
+    let span = document.createElement("span");
+        span.textContent = habitText;
+    let checkbox = document.createElement("input")
+        checkbox.type = "checkbox";
+    let buttonClear = document.createElement("button")
+        buttonClear.textContent="удалить";
+        buttonClear.addEventListener('click', function() {
+                li.remove();
+                saveHabits();
+        });
+        li.appendChild(checkbox);
+        li.appendChild(span);
+        li.appendChild(buttonClear);
+        list.appendChild(li); 
+
+}
+function saveHabits(){
+    let data = [];
+        document.querySelectorAll("li").forEach(function(li){
+            data.push(li.dataset.name);
+        });
+    localStorage.setItem('tasks', JSON.stringify(data))
+}
+function loadHabits(){
+    let saved = localStorage.getItem("tasks");
+    if (!saved) return;
+    let data = JSON.parse(saved);
+        data.forEach(function(habitText){
+            createHabitItem(habitText);
+
+    });
+}
+let list = document.getElementById('habit-list');
+
+let button = document.getElementById('btn-add').addEventListener('click', function() { 
+    let modal = document.querySelector('.modal-overlay');
+        modal.classList.add('active');
+});
+
+document.getElementById("btn-cancel").addEventListener('click', function() {
+    let modal = document.querySelector('.modal-overlay');
+        modal.classList.remove('active');
+});
+
+document.getElementById("btn-save").addEventListener('click', function() {
+    let habitText = document.querySelector('.modal__input').value;
+    if (!habitText || habitText.trim() === "") return;
+    createHabitItem(habitText);
+    saveHabits();
+    let modal = document.querySelector('.modal-overlay');
+        modal.classList.remove('active');
+    let textInpu = document.querySelector('.modal__input');
+        textInpu.value = '';
+});
+
+loadHabits();
